9eee0f05cb20c0c753cf9818a2c16bd046096a36
Johannes Mateboer <joh@nnes.site>
2022-08-19 06:55:33 +0200

    Updated to 1.18.x

--- src/main/java/net/simpletech/init/CustomTags.java
+++ src/main/java/net/simpletech/init/CustomTags.java
@@ -1,20 +1,19 @@
 package net.simpletech.init;
 
-import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import net.simpletech.SimpleTech;
 
 public class CustomTags {
-    public static final Tag<Item> TAGS_BIOS = TagRegistry.item(new Identifier(SimpleTech.MOD_ID, "bios"));
-    public static final Tag<Item> TAGS_COAL = TagRegistry.item(new Identifier(SimpleTech.MOD_ID, "coal"));
-    public static final Tag<Item> TAGS_DIAMONDS = TagRegistry.item(new Identifier(SimpleTech.MOD_ID, "diamonds"));
-    public static final Tag<Item> TAGS_DIRT_REMAINS = TagRegistry.item(new Identifier(SimpleTech.MOD_ID, "dirt_remains"));
-    public static final Tag<Item> TAGS_GOLD_INGOTS = TagRegistry.item(new Identifier(SimpleTech.MOD_ID, "gold_ingots"));
-    public static final Tag<Item> TAGS_LAPIS = TagRegistry.item(new Identifier(SimpleTech.MOD_ID, "lapis_lazulis"));
-    public static final Tag<Item> TAGS_REDSTONE = TagRegistry.item(new Identifier(SimpleTech.MOD_ID, "redstone_dusts"));
+    public static final TagKey<Block> TAGS_BIOS = TagKey.of(Registry.BLOCK_KEY, new Identifier(SimpleTech.MOD_ID, "bios"));
+    public static final TagKey<Block> TAGS_COAL = TagKey.of(Registry.BLOCK_KEY, new Identifier(SimpleTech.MOD_ID, "coal"));
+    public static final TagKey<Block> TAGS_DIAMONDS = TagKey.of(Registry.BLOCK_KEY, new Identifier(SimpleTech.MOD_ID, "diamonds"));
+    public static final TagKey<Block> TAGS_DIRT_REMAINS = TagKey.of(Registry.BLOCK_KEY, new Identifier(SimpleTech.MOD_ID, "dirt_remains"));
+    public static final TagKey<Block> TAGS_GOLD_INGOTS = TagKey.of(Registry.BLOCK_KEY, new Identifier(SimpleTech.MOD_ID, "gold_ingots"));
+    public static final TagKey<Block> TAGS_LAPIS = TagKey.of(Registry.BLOCK_KEY, new Identifier(SimpleTech.MOD_ID, "lapis_lazulis"));
+    public static final TagKey<Block> TAGS_REDSTONE = TagKey.of(Registry.BLOCK_KEY, new Identifier(SimpleTech.MOD_ID, "redstone_dusts"));
 
     public static void init() {
 
